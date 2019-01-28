52d2746524678c03f146142af3522aaa05cb1ee9
Luiz Augusto <luiz.moraes@drtis.com.br>
2019-01-27 23:25:49 -0200

    alterando retorno de dados

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const URL = `${process.env.API_URL}/books`;
 
 module.exports = {
-  list: async () => axios.get(URL),
-  get: async (id) => axios.get(`${URL}/${id}`),
-  create: async (data) => axios.post(URL, { ...data }),
+  list: async () => axios.get(URL).then(r => r.data),
+  get: async (id) => axios.get(`${URL}/${id}`).then(r => r.data),
+  create: async (data) => axios.post(URL, { ...data }).then(r => r.data),
 };

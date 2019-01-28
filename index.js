f80322cd3b1074b27392329d98a2c31ba9b78171
Luiz Augusto <luiz.moraes@drtis.com.br>
2019-01-28 01:30:22 -0200

    colocando linter com estilo de codigo do airbnb

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const URL = `${process.env.API_URL}/books`;
 
 module.exports = {
   list: async () => axios.get(URL).then(r => r.data),
-  get: async (id) => axios.get(`${URL}/${id}`).then(r => r.data),
-  create: async (data) => axios.post(URL, { ...data }).then(r => r.data),
+  get: async id => axios.get(`${URL}/${id}`).then(r => r.data),
+  create: async data => axios.post(URL, { ...data }).then(r => r.data),
 };
